468e47faf259300c4b3ddd894d0d372d9f2948c1
Brasther <sebastiansalamanca274@gmail.com>
2023-11-15 18:06:37 -0300

    commit 2 de info

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild,ElementRef} from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
 import { Preferences } from '@capacitor/preferences';
-
+import { Animation } from '@ionic/angular';
+import { AnimationController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  showPassword = false;
 
   formularioLogin: FormGroup;
+  @ViewChild('title', { read: ElementRef }) title!: ElementRef;
+  private animation!: Animation;
 
   constructor(public fb: FormBuilder,
-    public alertController: AlertController, private router: Router, public storage : StorageService) { 
+    public alertController: AlertController, private router: Router, public storage : StorageService, private animationCtrl: AnimationController ) { 
     this.formularioLogin = this.fb.group({
       'nombreLogin': new FormControl("", Validators.required), // Cambio de 'nombre' a 'nombreLogin'
       'password': new FormControl("", Validators.required)
@@ -25,6 +29,23 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  ngAfterViewInit(): void {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.title.nativeElement)
+      .duration(2500)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'translateX(0)', opacity: '1' },
+        { offset: 0.5, transform: 'translateX(100%)', opacity: '0.2' },
+        { offset: 1, transform: 'translateX(0)', opacity: '1' },
+      ]);
+    this.animation.play();
+  }
   async ingresar() {
     if (this.formularioLogin.valid) {
       const f = this.formularioLogin.value;
